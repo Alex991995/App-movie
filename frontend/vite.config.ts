3ac5315730bdc53be1cd798f79c8270f7ca036af
Alex991995 <sashe.1995@gmail.com>
2024-05-08 00:42:24 +0300

    fix proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server:{
+    // proxy: {
+    //   '/api': 'https://app-movie-backend.vercel.app'
+    // }
     proxy: {
-      // with options
       '/api': {
         target: 'https://app-movie-backend.vercel.app',
         changeOrigin: true,
-        secure: false
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   },
