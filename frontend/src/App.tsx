915d292483d814dc09d7f18590788fba0d5b6b90
Alex991995 <sashe.1995@gmail.com>
2024-05-08 23:50:07 +0300

    check deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,38 @@
 import { MantineProvider } from '@mantine/core';
 import NavBar from './components/NavBar';
 import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
-  // useEffect(() => {
-  //   fetch('/api/movie/2')
-  //     .then(data => data.json())
-  //     .then(res => setData(res))
-  //     .catch(err => console.error(err));
-  // }, []);
 
-  // useEffect(() => {
-  //   fetch('/api/genres')
-  //     .then(data => data.json())
-  //     .then(res => setGentes(res))
-  //     .catch(err => console.error(err));
-  // }, []);
+  const [data ,setData] = useState()
+  const [gen ,setGentes] = useState()
+  const [movie ,setMovies] = useState()
 
-  // useEffect(() => {
-  //   fetch('/api/movies')
-  //     .then(data => data.json())
-  //     .then(res => setGentes(res))
-  //     .catch(err => console.error(err));
-  // }, []);
+  useEffect(() => {
+    fetch('/api/movie/2')
+      .then(data => data.json())
+      .then(res => setData(res))
+      .catch(err => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    fetch('/api/genres')
+      .then(data => data.json())
+      .then(res => setGentes(res))
+      .catch(err => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    fetch('/api/movies')
+      .then(data => data.json())
+      .then(res => setMovies(res))
+      .catch(err => console.error(err));
+  }, []);
+
+  console.log(data)
+  console.log(gen)
+  console.log(movie)
 
   return (
     <MantineProvider>
